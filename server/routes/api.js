ab9f8ab261b9424f63e431ddb05bd3f7251ecbe9
nonomu <noammualmi@gmail.com>
2019-10-30 14:26:02 +0200

    basic files for the project

---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// const Person = require('../models/Person')
+
+
+module.exports = router
