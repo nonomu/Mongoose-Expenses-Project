c28460e7a152374ef9ac393f10369bedcb1c9ddd
nonomu <noammualmi@gmail.com>
2019-10-31 12:55:20 +0200

    added expenses + new expense post + update group for expense routes

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express()
 const api = require('./server/routes/api')
 const dbdata = require('./expenses')
+const Expense = require('./server/models/Expense')
 const bodyParser=require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 // Mongoose setup
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/ExpenseDb', { useNewUrlParser: true })
 
 app.use('/', api)
 
@@ -17,3 +18,9 @@ const port = 4200
 app.listen(port, function () {
     console.log(`Running on port ${port}`)
 })
+
+// Adding json db for my local DB
+// for (const d of dbdata) {
+//     const expense = new Expense(d)
+//     expense.save()
+// }
