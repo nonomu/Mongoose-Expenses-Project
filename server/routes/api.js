7231d982af281a92c0fcd04ab0bfd3b163243eaf
nonomu <noammualmi@gmail.com>
2019-10-31 16:14:37 +0200

    Get All Expenses For A Date Range

--- server/routes/api.js
+++ server/routes/api.js
@@ -24,39 +24,71 @@ router.post('/new', function (req, res) {
     const newExpense = new Expense(PostObj)
     newExpense.save().then(function () {
         Expense.count({}).exec(function (err, count) {
-            console.log("We have a " + count + " EXpenses");})
+            console.log("We have a " + count + " EXpenses");
         })
+    })
     res.end()
 })
 
 router.put('/update', function (req, res) {
-    Expense.findOneAndUpdate(
-        {group : `${req.body.group1}`},
-        { $set: { group:  `${req.body.group2}` } } ,
-        {new :true}   
-    ).exec(function (err, expense) {
-        res.send(expense._id + " changed to " +expense.group);
-    })
-    
+    Expense.findOneAndUpdate
+        (
+            { group: `${req.body.group1}` },
+            { $set: { group: `${req.body.group2}` } },
+            { new: true }
+        )
+        .exec(function (err, expense) {
+            res.send(expense._id + " changed to " + expense.group);
+        })
+
 })
 router.get('/expenses/:group', function (req, res) {
-     if(req.query.total)
-     {
-        Expense.aggregate([
-            { $match: { group: `${req.params.group}` } },
-            { $group: { _id: "$group", total: { $sum: "$amount" } } }
-          ]).exec(function (err, totalAmount) {
-            if(err){res.send(err.errmsg)}
-             if(!totalAmount.length){ return res.send("Empty") }
-               
-            console.log(totalAmount[0].total)
-              let total=totalAmount[0].total
-            res.send (total.toString())
-          })
-     }
-     else{
-    Expense.find({group: `${req.params.group}`}).exec(function (err, expensesByGroup) {
-        res.send(expensesByGroup)
-    })}
+    let d1 = req.query.d1 ? moment(req.query.d1).format("LLLL") : moment().format("LLLL")
+    let d2 = req.query.d2 ? moment(req.query.d2).format("LLLL") : moment().format("LLLL")
+
+    if (req.query.total == "true") {
+        Expense.aggregate
+            (
+                [
+                    { $match: { group: `${req.params.group}` } },
+                    { $group: { _id: "$group", total: { $sum: "$amount" } } }
+                ]
+            )
+            .exec(function (err, totalAmount) {
+                if (err) { res.send(err.errmsg) }
+                if (!totalAmount.length) { return res.send("Empty") }
+                let total = totalAmount[0].total
+                res.send(total.toString())
+            })
+    }
+    else if(d1) {
+        console.log(d1);
+        console.log(d2);
+        Expense.find({
+            $and: [
+                { date: { $gte: d1 } },
+                { date: { $lte: d2 } }
+            ]
+        }).exec(function (err, data) {
+            res.send(data);
+        })
+    }
+    else {
+        Expense.find({ group: `${req.params.group}` }).exec(function (err, expensesByGroup) {
+            res.send(expensesByGroup)
+        })
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router
